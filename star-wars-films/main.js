bdc3a2abe59dbb4ffd26f9c3752f340c929f5bf9
Gordon Reid <nodrog117@gmail.com>
2020-11-17 12:53:44 -0700

    trying to fix films page

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -4,7 +4,7 @@ import { getLastNumber } from '../utils/index.js'
 const main = document.querySelector('main')
 
 for (let i = 0; i < 7; i++) {
-    let figure = document.createE+ement('figure')
+    let figure = document.createElement('figure')
     let figImg = document.createElement('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figcaption')
